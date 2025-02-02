bd3e74a946e75bb763e0933d73e10e325afdaaaf
Krzysztof Wilgucki <128691376+BrightIndigo@users.noreply.github.com>
2025-02-02 23:58:01 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,32 +1,33 @@
-from PyQt6.QtCore import *
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
+from PyQt6.QtCore import QDate
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QCalendarWidget, QLabel, QPushButton, QMessageBox
 import sys
-import datetime
 
 class Window(QWidget):
-    
     def __init__(self):
         super().__init__()
-        
         self.calendar_window()
         
     def calendar_window(self):
-        layout =  QVBoxLayout(self)
+        layout = QVBoxLayout(self)
         
-        #calendar widget
-        calendar = QCalendarWidget(self)
-        calendar.setGridVisible(True)
-        calendar.clicked[QDate].connect(self.showDate)
-        layout.addWidget(calendar)
+        # Calendar widget
+        self.calendar = QCalendarWidget(self)
+        self.calendar.setGridVisible(True)
+        self.calendar.clicked.connect(self.showDate)
+        layout.addWidget(self.calendar)
         
-        #label
+        # Label to display selected date
         self.label = QLabel(self)
-        date = calendar.selectedDate()
+        date = self.calendar.selectedDate()
         self.label.setText(date.toString())
         layout.addWidget(self.label)
         
-        #layout
+        # Button to trigger an action
+        self.button = QPushButton("Show Selected Date", self)
+        self.button.clicked.connect(self.showMessageBox)
+        layout.addWidget(self.button)
+        
+        # Window settings
         self.setLayout(layout)
         self.setGeometry(300, 300, 350, 300)
         self.setWindowTitle("Calendar")
@@ -34,14 +35,14 @@ class Window(QWidget):
         
     def showDate(self, date):
         self.label.setText(date.toString())
-        
-        
+
+    def showMessageBox(self):
+        selected_date = self.calendar.selectedDate().toString()
+        QMessageBox.information(self, "Selected Date", f"You selected: {selected_date}")
+
 def main():
     app = QApplication(sys.argv)
     win = Window()
     sys.exit(app.exec())
 
 main()
-        
-        
-        
